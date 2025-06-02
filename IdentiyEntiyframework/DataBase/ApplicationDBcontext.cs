cfd464dc196e53df622c2b50bc40f619a6378626
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-02 12:50:36 +0530

    Initial commit wwithout .vs files

---
+++ IdentiyEntiyframework/DataBase/ApplicationDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentiyEntiyframework.DataBase
+{
+    public class ApplicationDBcontext: IdentityDbContext
+    {
+        public ApplicationDBcontext(DbContextOptions Options)
+            :base(Options)
+        {
+            
+        }
+    }
+}
