e3753930ec5e619d8980a2eca7ecc947839e90dc
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-02 16:48:55 +0530

    register action method in acoount coontroller register first user

--- IdentiyEntiyframework/Controllers/AccountController.cs
+++ IdentiyEntiyframework/Controllers/AccountController.cs
@@ -1,14 +1,49 @@
-﻿using IdentiyEntiyframework.Models.ViewModels;
+﻿using IdentiyEntiyframework.Models;
+using IdentiyEntiyframework.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace IdentiyEntiyframework.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<Applicationuser> _userManager;
+        
+        private readonly SignInManager<Applicationuser> _signInManager;
+
+        public AccountController(UserManager<Applicationuser> userManager, 
+             SignInManager<Applicationuser> signInManager
+            )
+        {
+            
+        }
         public IActionResult Register()
         {
             RegisterViewModel registerViewModel = new();
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Applicationuser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded) { 
+                 await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
         }
     }
 }
