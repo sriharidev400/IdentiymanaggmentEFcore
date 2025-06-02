c57f585b066e38b9c55f34e9e06487b7398c0219
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-02 16:12:26 +0530

    viewmodels loginview model register view model

---
+++ IdentiyEntiyframework/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using IdentiyEntiyframework.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentiyEntiyframework.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            RegisterViewModel registerViewModel = new();
+            return View(registerViewModel);
+        }
+    }
+}
