3f9cd6339eb6546b9f3ef440f76c2c6508d3a48b
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-02 15:28:58 +0530

    create a Register and LoginviewModel in model folder

---
+++ IdentiyEntiyframework/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentiyEntiyframework.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Rember me?")]
+        public bool RemberMe { get; set; }
+    }
+}
---
+++ IdentiyEntiyframework/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentiyEntiyframework.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(100,ErrorMessage ="The {0} must be at least  {2} charcater long",MinimumLength =6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="confirm password")]
+        [Compare("Password",ErrorMessage ="The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
