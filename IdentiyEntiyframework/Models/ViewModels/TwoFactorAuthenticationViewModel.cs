26b8f4508a4a89e80cb6d6788ada6a6f60b42fb7
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-04 16:01:05 +0530

    Enable Authenticator Getendpoint

---
+++ IdentiyEntiyframework/Models/ViewModels/TwoFactorAuthenticationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentiyEntiyframework.Models.ViewModels
+{
+    public class TwoFactorAuthenticationViewModel
+    {
+        
+        public string Code { get; set; }
+        public string Token { get; set; }
+
+    }
+}
