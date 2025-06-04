f43b83f879ea5d3a739f14214833981c4f094bb2
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-04 21:28:21 +0530

    Reset two factor RemoveAuthenticato

--- IdentiyEntiyframework/Controllers/HomeController.cs
+++ IdentiyEntiyframework/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using IdentiyEntiyframework.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentiyEntiyframework.Controllers
@@ -8,15 +10,26 @@ namespace IdentiyEntiyframework.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<Applicationuser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,UserManager<Applicationuser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            if(user == null)
+            {
+                ViewData["TwoFactorEnabled"] = false;
+            }
+            else
+            {
+                ViewData["TwoFactorEnabled"] = user.TwoFactorEnabled;
+            }
+                return View();
         }
 
         [Authorize]
