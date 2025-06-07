6bb37fa61f511b09b8d1b5a023ddc52caf1af257
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-07 21:33:48 +0530

    get the roles in database display the Role List

---
+++ IdentiyEntiyframework/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using IdentiyEntiyframework.DataBase;
+using IdentiyEntiyframework.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging.Core;
+
+namespace IdentiyEntiyframework.Controllers
+{
+    public class RoleController : Controller
+    {
+        
+        private readonly ApplicationDBcontext  _db;
+        private readonly UserManager<Applicationuser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(ApplicationDBcontext db,
+            UserManager<Applicationuser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            
+            var roles = _db.Roles.ToList();
+           
+
+            return View(roles);
+        }
+    }
+}
