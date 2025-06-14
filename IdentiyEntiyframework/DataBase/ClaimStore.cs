19fbdd17c0e1abd53a1f3a7c9af6a92dfd9978e4
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-14 19:05:29 +0530

    Claim store

---
+++ IdentiyEntiyframework/DataBase/ClaimStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace IdentiyEntiyframework.DataBase
+{
+    public  static class ClaimStore
+    {
+        public static List<Claim> claimsList = 
+            [
+            new Claim("Create","Create"),
+            new Claim("Edit","Edit"),
+            new Claim("Delete","Delete")
+            ];
+    }
+}
