cee95b518e29e71d501e6190256d6a320173d81f
demolearningaz204 <demolearningaz204@gmail.com>
2025-06-22 16:03:38 +0530

    Setup Project for Access checker

---
+++ IdentiyEntiyframework/Controllers/AccessCheckerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentiyEntiyframework.Controllers
+{
+    public class AccessCheckerController : Controller
+    {
+        //Any one can access this
+        public IActionResult AllAccess()
+        {
+            return View();
+        }
+        //Any one that has logged in can access
+        public IActionResult AuthorizedAccess()
+        {
+            return View();
+        }
+        //account  with role of user can access
+        public IActionResult UserRoleAccess()
+        {
+            return View();
+        }
+        //account with role of admin can access
+        public IActionResult AdminRoleAccess()
+        {
+            return View();
+        }
+        // account with admin role and create claim can  access
+        public IActionResult AdminCreateAccess()
+        {
+            return View();
+        }
+        // account with admin role and (create and edit and delete) claim can  access
+        public IActionResult Admin_Create_Edit_DeleteAccess()
+        {
+            return View();
+        }
+    }
+}
